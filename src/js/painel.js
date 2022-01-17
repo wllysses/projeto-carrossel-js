f41e7e5115142d30e645dbcfc74b081c11a30d08
Wllysses Jr <wtpj@hotmail.com>
2022-01-17 20:27:24 -0300

    first commit

---
+++ src/js/painel.js
@@ -0,0 +1,38 @@
+const imagensPainel = document.querySelectorAll('.imagen-painel')
+const setaVoltar = document.getElementById('btn-voltar')
+const setaAvancar = document.getElementById('btn-avancar')
+let imagemAtual = 0
+
+function esconderImagem() {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    })
+}
+
+function mostrarImagem() {
+    imagensPainel[imagemAtual].classList.add('mostrar')
+}
+
+setaAvancar.addEventListener('click', function() {
+
+    if (imagemAtual === imagensPainel.length - 1) {
+        return
+    }
+
+    imagemAtual++
+
+    esconderImagem()
+    mostrarImagem()
+})
+
+setaVoltar.addEventListener('click', function() {
+
+    if (imagemAtual === 0) {
+        return
+    }
+
+    imagemAtual--
+
+    esconderImagem()
+    mostrarImagem()
+})
